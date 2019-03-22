f021ba0cc3347edb78103bfd7fed88636bff1fb7
John Hocking <jhocking@TN.local>
2019-03-22 12:48:00 -0500

    Unit Tests

---
+++ CreditCardInterest/CalcCreditCardInterest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreditCardInterest
+{
+    public class CreditCardInterest
+    {
+         public static double CalcInterest(string CardType, double Amount)
+        {
+            double Rate = 0;
+            switch (CardType)
+            {
+                case "Visa": Rate = 0.1;
+                    break;
+                case "MC": Rate = 0.05;
+                    break;
+                case "Discover": Rate = 0.01;
+                    break;
+                   
+            }
+            return Amount * Rate;
+        }
+
+       
+    }
+}
---
+++ CreditCardInterestTests/CreditCardInterestTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CreditCardInterestTests
+{
+    [TestClass]
+    public class CreditCardInterestTests
+    {
+        [TestMethod]
+        public void OneWalletThreeCards_VisaMCdDiscover_Returns16()
+        {
+            double Results = 0;
+            //Wallet 1
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("Visa",100);
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("MC", 100);
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("Discover", 100);
+            Assert.AreEqual(16,Results);
+        }
+
+        [TestMethod]
+        public void TwoWalletsThreeCards_VisaMCDiscover_Returns10and6()
+        {
+            double Results = 0;
+            //Wallet 1 
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("Visa", 100);
+            Assert.AreEqual(10, Results);
+            
+            //Wallet 2
+            Results = 0;
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("Discover", 100);
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("MC", 100);
+            Assert.AreEqual(6, Results);
+        }
+
+        [TestMethod]
+        public void TwoPeopleTwoWalletsTwoCardsEach_VisaMC_Returns15()
+        {
+            double Results = 0;
+            //Wallet 1 
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("Visa", 100);
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("MC", 100);
+            Assert.AreEqual(15, Results);
+
+            //Wallet 2
+            Results = 0;
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("Visa", 100);
+            Results = Results + CreditCardInterest.CreditCardInterest.CalcInterest("MC", 100);
+            Assert.AreEqual(15, Results);
+        }
+    }
+}
